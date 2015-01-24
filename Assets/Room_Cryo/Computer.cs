793d80d0329c3f284b6bf10cc7be75efd0d27f26
Juan Corona <juanc@evidentpoint.com>
2015-01-24 02:03:04 -0800

    Implement interacting with objects.

---
+++ Assets/Room_Cryo/Computer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Computer : MonoBehaviour, IInteractable {
+
+	public GameObject terminalObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnInteract() {
+		terminalObject.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Transform MainCamera;
+
+
+	public float hitRadius = 1f;
+	public float hitDistace = 0.6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Action")) {
+			PerformAction();
+		}
+	}
+
+	void PerformAction() {
+		Transform cam = MainCamera.transform;
+		var ray = new Ray(cam.position, cam.forward);
+		
+		RaycastHit hit;
+		var radius = hitRadius;
+		var distance = hitDistace;
+
+		if (Physics.SphereCast(ray, radius, out hit, distance, 1 << LayerMask.NameToLayer("Interactables"))) {
+			IInteractable interactable = hit.collider.gameObject.GetComponent(typeof(IInteractable)) as IInteractable;
+			interactable.OnInteract();
+		}
+	}
+}
