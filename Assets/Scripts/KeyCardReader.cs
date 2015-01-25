ea9dd2a04c53a63b314e8569eee14c47b959d2bb
Juan Corona <juanc@evidentpoint.com>
2015-01-25 05:39:11 -0800

    Forgot to include things in last commit.

---
+++ Assets/Scripts/KeyCardReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyCardReader : MonoBehaviour, IInteractable {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void OnInteract () {
+        if (PlayerStateManager.instance.hasKeyCard && PlayerStateManager.instance.isAdmin) {
+            GameManager.instance.UnlockBridge ();
+        }
+    }
+}
